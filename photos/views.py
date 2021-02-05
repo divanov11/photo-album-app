c1e6194eacf196eb09d77869cffb30138875b164
divanov11 <ivanovsin11@gmail.com>
2021-02-05 09:21:05 -0800

    Added demo bucket policy

--- photos/views.py
+++ photos/views.py
@@ -25,7 +25,7 @@ def addPhoto(request):
 
     if request.method == 'POST':
         data = request.POST
-        image = request.FILES.get('image')
+        images = request.FILES.getlist('images')
 
         if data['category'] != 'none':
             category = Category.objects.get(id=data['category'])
@@ -35,11 +35,12 @@ def addPhoto(request):
         else:
             category = None
 
-        photo = Photo.objects.create(
-            category=category,
-            description=data['description'],
-            image=image,
-        )
+        for image in images:
+            photo = Photo.objects.create(
+                category=category,
+                description=data['description'],
+                image=image,
+            )
 
         return redirect('gallery')
 
